c9e07024b0610537cf374be660a3f654bf17d966
shark426Develop <gustavolpz16@gmail.com>
2021-03-10 16:43:35 -0500

    proyecto de rasnportes para empresa de carga

---
+++ src/main/java/trnsptng/TrnsptngApplication.java
@@ -0,0 +1,13 @@
+package trnsptng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrnsptngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrnsptngApplication.class, args);
+	}
+
+}
